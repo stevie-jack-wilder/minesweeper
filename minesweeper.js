52379ef6a3a4be6d3689b7cbda51d3fe69cbe65c
stevie-jack-wilder <steviejwilder@gmail.com>
2020-07-22 11:16:13 +1200

    Work on startGame function, list problems to solve and areas to ask for help with.

--- minesweeper.js
+++ minesweeper.js
@@ -1,87 +1,69 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// Define your `board` object here!
+//Board is an object literal. It contains a single array of 'cells'. (array identifier - [] )
+//Within the array, we have sub-objects definining the physical position of the nine board boxes. (object identifier - {} )
+//Within the array, we have booleans, declaring is that board box has a mine and if the box is hidden. (true/false)
 
-//var board is an object containing one property - cells.
-//cells is an array containing nine sub-objects. 
-//each sub-object has multiple properties. 
-//row and col values defined by physical position on the board.
-//What is isMarked??? No instruction to include. 
 
 var board = {
   cells: [
-    {
-      row: 0, col: 0, 
+    {row: 0, col: 0, 
       isMine: true,
       hidden: true,
-      surroundingMines: 0,
     },
-
-    {
-      row: 0, col: 1, 
+    {row: 0, col: 1, 
       isMine: false,
       hidden: true,
-      surroundingMines: 2,
     }, 
-
-    {
-      row: 0, col: 2, 
+    {row: 0, col: 2, 
       isMine: false,
       hidden: true,
-      surroundingMines: 1,
     }, 
-
-    {
-      row: 1, col: 0, 
+    {row: 1, col: 0, 
       isMine: false,
       hidden: true,
-      surroundingMines: 1,
     },
-
-    {
-      row: 1, col: 1, 
+    {row: 1, col: 1, 
       isMine: false,
       hidden: true,
-      surroundingMines: 2,
     }, 
-
-    {
-      row: 1, col: 2, 
+    {row: 1, col: 2, 
       isMine: true,
       hidden: true,
-      surroundingMines: 1,
     },
-
-    {
-      row: 2, col: 0, 
+    {row: 2, col: 0, 
       isMine: false,
       hidden: true,
-      surroundingMines: 0,
     },
-
-    {
-      row: 2, col: 1, 
+    {row: 2, col: 1, 
       isMine: false,
       hidden: true,
-      surroundingMines: 2,
     }, 
-
-    {
-      row: 2, col: 2, 
+    {row: 2, col: 2, 
       isMine: true,
       hidden: true,
-      surroundingMines: 1,
     }, 
+
   ]
+
 }
+/* PROBLEM SOLVING NOTES:
 
-//for loop iterates by one until it reaches zero. 
-//result of loop attaches to countSurroundingMines.
+1- Is surroundingMines last in the for loop call instruction because it is further down the body of the code? Initially I would not thought to have done this, as surroundingMines is a seperate function, and board, cells is it's own var of objects. I assumed that it would not connect....ah, is it because Board is a global variable?
+
+2 - If I didn't have let' after 'for' in startGame function, would it still work? Not far through this challenge yet to test.... I think.
+
+3 - "The new property should be called surroundingMines" - Do we add this property into the cells arrays manually as I have done with hidden:true, for example?
+
+*/
+
+
 function startGame () {
-  for (var i = 0; i < board.cells.length; i++) {
-   board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  for (let i = 0; i < board.cells.length; i++) 
+  {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
-
+  // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
 
