15b786cd8fe24520d63df685b6f29d6dde3dfc6a
stevie-jack-wilder <steviejwilder@gmail.com>
2020-07-22 19:21:24 +1200

    addEventListener x2 - startGame func.

--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-
-
 //Board is an object literal. It contains a single array of 'cells'. (array identifier - [] )
 
-//Within the array, we have sub-objects definining the physical position of the nine board positions (cells). (object identifier - {} )
-
-//Within the cells array, we have booleans, declaring if a cell has a mine and if the box is hidden. (true/false)
+//Within the array, we have sub-objects with four properties and values, which define the physical position of the nine board positions (cells), tells us if a cell holds a mine, and if the cell is hidden. (object identifier - {} )
 
 
 var board = {
@@ -59,34 +55,17 @@ var board = {
   ]
 
 }
-/* PROBLEM SOLVING - NEED GUIDANCE:
 
-0 - Is my formatting of row and col on one line okay? It makes more 'visual' sense to me in understanding the physical layout. 
-
-0.5 - General question, is there a way to have the location of mines change using the math.random function. 
-
-1- Is surroundingMines last in the for loop call instruction because it is further down the body of the code? Initially I would not thought to have done this, as surroundingMines is a seperate function, and board, cells is it's own var of objects. I assumed that it would not connect....ah, is it because Board is a global variable?
-
-2 - If I didn't have let' after 'for' in startGame function, would it still work? Not far through this challenge yet to test.... I think.
-
-3 - "The new property should be called surroundingMines" - Do we add this property into the cells arrays manually as I have done with hidden:true, for example? Or, do we 'push' it to the sub-objects of the cells array?
-
-4-  countSurroundingMine function example has 'cell' instead of cells when identifying rows and columns. Surely this will not work as the array for board is called Cells (plural) or is this identifying a different variable called cell?
-
-5 - Unsure how to loop through surrounding cells returned from getSurroundingCells, checking each to see if it is a mine and adding to a count variable if it is.
-    - Need a for loop (if/else)?
-    - Need a boolean for cells.isMine (true/false)?
-    - 
-    - 
-
-*/
-
+// startGame iterates loop by one until no longer true.
+// ...Creates surroundingMines var, assigns result of loop to countSurroundingMines function.
+// ...assigns EventListeners to left and right mouse click, and calls checkForWin function each time EventListener is triggered.
 
 function startGame () {
   for (let i = 0; i < board.cells.length; i++) {
    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+   document.addEventListener("click", checkForWin);
+   document.addEventListener("contextmenu", checkForWin);
   }
-  console.log(board.cells)
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
